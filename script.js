27189aa55b4defce2775b0a9d6363c84e16298d9
SrJoshua <107871841+SrJoshua@users.noreply.github.com>
2026-07-20 22:47:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,31 +6,46 @@ boton.addEventListener("click", buscarPersona);
 async function buscarPersona() {
 
     const cedula = document.getElementById("cedula").value.trim();
-    const local = document.getElementById("local").value;
 
     if (cedula === "") {
         alert("Ingrese una cédula.");
         return;
     }
 
-    const respuesta = await fetch(`datos/${local}.json`);
-    const personas = await respuesta.json();
+    // Lista de todas las planillas
+    const archivos = [
+        "datos/loma_merlo.json",
+        "datos/manuel_dominguez.json"
+    ];
 
-    const persona = personas.find(p => p.cedula == cedula);
+    let personaEncontrada = null;
 
-    if (persona) {
+    for (const archivo of archivos) {
+
+        const respuesta = await fetch(archivo);
+        const personas = await respuesta.json();
+
+        personaEncontrada = personas.find(p => p.cedula == cedula);
+
+        if (personaEncontrada) {
+            break;
+        }
+
+    }
+
+    if (personaEncontrada) {
 
         resultado.innerHTML = `
             <div class="card">
-                <h2>${persona.nombre}</h2>
+                <h2>${personaEncontrada.nombre}</h2>
 
-                <p><strong>C.I.</strong> ${persona.cedula}</p>
+                <p><strong>C.I.</strong> ${personaEncontrada.cedula}</p>
 
-                <p><strong>Mesa:</strong> ${persona.mesa}</p>
+                <p><strong>Mesa:</strong> ${personaEncontrada.mesa}</p>
 
-                <p><strong>Orden:</strong> ${persona.orden}</p>
+                <p><strong>Orden:</strong> ${personaEncontrada.orden}</p>
 
-                <p><strong>Local:</strong> ${persona.local}</p>
+                <p><strong>Local:</strong> ${personaEncontrada.local}</p>
 
             </div>
         `;
